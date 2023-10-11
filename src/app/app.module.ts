09433a20bf8290e83ec870883444c7319ce223cd
MaksimYakovenko <maksim13yakoveno@gmail.com>
2023-10-11 17:11:51 +0300

    commit init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BigdataFibAngularComponent } from './bigdata-fib-angular/bigdata-fib-angular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BigdataFibAngularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
